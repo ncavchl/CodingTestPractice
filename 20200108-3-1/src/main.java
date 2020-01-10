22308f1604f5fb900f8313226da430d821afdc40
ncavchl <ncavchl@ajou.ac.kr>
2020-01-10 14:00:56 +0900

    백준 5568 카드놓기

--- 20200108-3-1/src/main.java
+++ 20200108-3-1/src/main.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+
+import org.omg.CosNaming.NamingContextExtPackage.AddressHelper;
+
 //import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Queue;
@@ -9,6 +12,7 @@ import java.util.List;
 import java.util.Stack;
 import java.lang.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.File;
@@ -16,113 +20,43 @@ import java.io.File;
 //import java.util.*;
 
 /*
- 백준  1644 소수의 연속합 
+ 백준  1837 골드바흐의 추측
  */
 public class main {
-   
-   static int N; //1~1000000 수의 개수 
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        final int N = 100;
+        boolean [] b = new boolean[N+1];
+        List<Integer> list = new ArrayList<>();
+ 
+        Arrays.fill(b, true);
+ 
+        for(int i=3; i*i <= N; i+=2) {
+            if(b[i]) //똑같은 배수를 돌 필요는 없다. i=4일 때 4의 배수는 2의 배수
+                for(int j=i*i; j<=N; j+=i) {
+                    b[j] = false;
+                    System.out.println(j);
+                    if(j==6) System.out.println(6 + " - ");
+                }    
+        }
+        for(int i=3; i<=N; i+=2)
+            if(b[i])
+                list.add(i);
+ 
+        while(true) {
+            int n = scan.nextInt();
+            boolean isWrong = true;
+            if(n==0)
+                break;
+            for(int i=0; i<list.size(); i++)
+                if(b[n-list.get(i)]) {
+                    System.out.println(n+ " = " + list.get(i) +" + " + (n-list.get(i)));
+                    isWrong = false;
+                    break;
+                }
+            if(isWrong) //입력만 정상적이라면 틀릴 경우는 사실 없다.
+                System.out.println("Goldbach's conjecture is wrong.");
+        }
+    }
 
-   
-   
-   public static void main(String args[]) throws Exception {
-      //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      //StringTokenizer st = new StringTokenizer(br.readLine()); 
-      
-	  Scanner sc = new Scanner(System.in);
-     // N = Integer.parseInt(st.nextToken());
-   
-      N = sc.nextInt();
-     
-      ArrayList<Integer> di = new ArrayList<Integer> (); // 소수 담긴 리스트 
-
-      int index = 0;
-
-      if(N == 1) {
-    	  System.out.println(0); // 1은 소수가 아니므로 0 출력
-    	  return;
-      }
-      
-      di = getprime(N);
-   
-     // System.out.println(di.size() + "-");
-
-     int len = di.size();
-     int cnt = 0;
-     
-     int left = 0;
-     int right = 0;
-     int sum = di.get(0); // 처음값으로세팅
-     int size = len;
-     int last = size - 1;
-     while(left <= right && right < size) {
-    	 if(sum >= N ) {
-    		 if(sum == N) {
-    			 cnt++;
-    			
-    			 System.out.println(left + " " + di.get(left) +" -" + right + " " + di.get(right));
-    		}
-    		 if(left+1<size) {
-    			 
-    			 sum -= di.get(left);
-    		 }
-    		 left++;
-    		 continue;
-    	 }
-    	 if(sum < N) {
-    		 if(right+1<size)
-    			 sum += di.get(right+1);
-    		 right++;
-    		 continue;
-    	 }
-     }
-     
-     //  if(prime(N) == true ) cnt++;
-     
-     System.out.println(cnt + " ");
-      
-      
-     
-   }
-   
-
-   
-   public static ArrayList getprime(int N) {
-	   ArrayList<Integer> arr = new ArrayList<Integer> ();
-	   int[] visit = new int[N+2];
-	   
-	   int index=2;
-		int end = 0;
-		int result = 0;
-		int check = 0;
-		while(index <= N) {
-			//System.out.println("1while " + index );
-	
-			if(visit[index] == 0) {
-				
-				arr.add(index);
-				//System.out.println("-" + index);
-				
-				int i=1;
-				int delete =index;
-				
-				while(delete<= N) {
-					
-					if(visit[delete] == 0) {
-						visit[delete] = 1;					
-						delete = index * i;						
-						check++;
-					}
-					i++;
-					delete = index * i;
-
-				}
-			}
-			
-			index++;
-		}	   
-	   
-	   return arr;
-   }
-   
-
-}   
+}
