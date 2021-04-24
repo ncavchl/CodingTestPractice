a4d9f90d0a3376c9d435cfe06ffe286f83a7030d
ncavchl <ncavchl@hyundai-autoever.com>
2021-04-24 12:14:41 +0900

    setting

---
+++ 2021/code-t/code-t/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace code_t
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
